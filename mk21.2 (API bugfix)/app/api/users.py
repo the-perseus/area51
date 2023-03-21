4efd971d576e2fcc8feb0e9c5d3da15f57d08a85
the-perseus <127565943+the-perseus@users.noreply.github.com>
2023-03-21 21:26:58 +0100

    API bugfix

---
+++ mk21.2 (API bugfix)/app/api/users.py
@@ -0,0 +1,21 @@
+from flask import jsonify, request, url_for, abort
+from app import db
+from app.models import User
+from app.api import bp
+from app.api.auth import token_auth
+from app.api.errors import bad_request
+
+
+@bp.route('/users/<int:id>', methods=['GET'])
+@token_auth.login_required
+def get_user(id):
+    return jsonify(User.query.get_or_404(id).to_dict())
+
+
+@bp.route('/users', methods=['GET'])
+@token_auth.login_required
+def get_users():
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
+    return jsonify(data)
