51138eac778501a599363748906fed28ff553ee1
the-perseus <127565943+the-perseus@users.noreply.github.com>
2023-03-22 21:09:40 +0100

    API bugfix

---
+++ mk21.4 (API bugfix)/app/api.py
@@ -0,0 +1,13 @@
+# app/api.py
+from app import app
+from app.models import User, Post
+from flask import jsonify
+
+@app.route('/api/users/<int:id>', methods=['GET'])
+def get_user(id):
+    data = User.query.get_or_404(id).to_dict()
+    return jsonify(data)
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    data = User.to_collection()
+    return jsonify(data)
